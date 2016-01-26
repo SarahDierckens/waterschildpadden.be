914e13c47b34fe64e2c0f4807e6836ea9e21ee82
easybird <easybird85@gmail.com>
2016-01-26 10:08:07 +0100

    complete indexPage + introduce dynamic routing for mainPages

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,26 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Waterschildpadden' });
+  res.render('pages/indexPage', { title: 'Waterschildpadden' });
 });
 
 /* GET home page. */
 router.get('/*', function(req, res, next) {
-    res.render('mainPage', { title: 'Waterschildpadden' });
+    res.render('pages/introPage', {
+        title: 'Waterschildpadden',
+        content: getPageContent(req.path)
+    });
 });
 
 module.exports = router;
+
+
+//---------------helper functions-----------------------//
+function getPageContent(path) {
+    var navigation = trimPath(path);
+    return require('../data/content.json')[navigation];
+
+    function trimPath(path) {
+        return path.substring(path.lastIndexOf('/')+1);
+    }
+}
